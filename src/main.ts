56948aeeb327929e2135b78c3e98a53de4269bd3
jonathanjoelneptune <jonathanjoelneptune@gmail.com>
2026-05-29 04:18:25 -0400

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -434,7 +434,7 @@ const DEFAULT_ROOM_UTILITY: RoomUtilitySettings = {
   lyricPosterRowBreakpoint: 28,
   lyricPosterTransition: "none"};
 
-const ROOM_UTILITY_KEY = "pocketdj-room-utility-v64d";
+const ROOM_UTILITY_KEY = "pocketdj-room-utility-v64e";
 let roomUtility = loadRoomUtilitySettings();
 
 
